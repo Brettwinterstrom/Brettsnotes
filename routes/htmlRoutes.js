fb0f0ef760028936985ef86c31335b2b6b44ba29
Brett Winterstrom <brettwinterstrom@yahoo.com>
2020-12-18 18:49:50 -0800

    set up express server and set up HTML routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,12 @@
 const path = require("path");
-const app = require("express");
+const app = require("express").Router();
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = app;
