1518e2c956bcb9ce3fc2bfef7e62d05d385d30cf
Brett Winterstrom <brettwinterstrom@yahoo.com>
2020-12-18 19:08:59 -0800

    set up server.js and added dependencies to apiroutes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,4 @@
+//Dependencies  
+const fs = require("fs");
+const app = require("express").Router();
+const path = require("path");
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
 app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 app.listen(PORT, function () {
     console.log(`Listening on PORT: ${PORT}`);
-});
+});  
